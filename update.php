2d7d4a9562fce8c7df2488db1d107678754a9973
lazmakelabs <lazarusmakelabs@gmail.com>
2024-07-24 11:10:06 +0530

    update front end

--- update.php
+++ update.php
@@ -4,28 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <div class="form-container1">
-        <!-- <img src="make labs logo.png" alt=""> -->
+<form action="status.php" method="post">
+        <div class="container1">
         <div class="status">
-            <a href="view.php" >Back</a>
+            <a href="middle.php" >Back</a>
         </div>
-        <form method="post" action="Includes/update.inc.php">
-            <div class="mode">
-                <h2>&emsp;&emsp;&emsp;&nbsp;Update</h2>
-                <div class="input-field">
-                    <label>Payment amount</label>
-                    <input type="text" placeholder="Enter amount" name="pay_amt" required>
-                </div> 
-                <div class="input-field">
-                    <label>Payment date</label>
-                    <input type="date" placeholder="Enter date" name="pay_date" required>
+            <div class="form first1">
+            <h2>Update</h2>
+                <div class="details personal1">
+                    <div class="fields">
+                        <div class="input-field1">
+                            <label>Payment Amount</label>
+                            <input type="number" placeholder="Enter amount" name="pay_amt" required>
+                        </div>
+                        <div class="input-field1">
+                            <label>Date</label>
+                            <input type="date" placeholder="Date" name="pay_date" required>
+                        </div>
+                    </div>
                 </div>
+                <button class="nextBtn1" type="submit">
+                    <span class="btnText1">Submit</span>
+                </button>
             </div>
-       </form>
-    </div>
+        </div>    
+    </form>
 </body>
 </html>
 
